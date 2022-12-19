2da947e9563c053e90e651ed39054ebf28fdd8cd
Cubiii <120466698+CubesAndCubes@users.noreply.github.com>
2022-12-19 03:33:38 +0100

    Bump version

--- anatomize.js
+++ anatomize.js
@@ -1,5 +1,5 @@
 /*
-    Anatomize.js v1.1.0 | A JavaScript-based framework for building parsers
+    Anatomize.js v1.1.1 | A JavaScript-based framework for building parsers
     Copyright (C) 2022  CubesAndCubes
 
     Anatomize is free software: you can redistribute it and/or modify
